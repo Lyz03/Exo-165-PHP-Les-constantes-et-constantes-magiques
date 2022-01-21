5082d62fa77d251905b827d9ea5471af37c03566
Lyz03 <lizoe@netcourrier.com>
2022-01-21 13:42:12 +0100

    exo 165

--- index.php
+++ index.php
@@ -9,9 +9,19 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const CONST1 = 12;
+const CONST2 = 2;
 
+echo "<p>" . CONST1 * CONST2 . "</p>";
+
+const CONST3 = "C'est cool PHP";
+
+echo "<p>" . (CONST1 + CONST2) * strlen(CONST3) . "</p>";
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+echo __FILE__ . "<br>";
+echo __DIR__ . "<br>";
+echo __LINE__ . "<br>";
